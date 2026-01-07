f6c9aaaa7abbe8626b959459cb102e0cd01f8520
dimc0 <dimitri.camet@efrei.net>
2026-01-07 10:56:24 +0100

    modification

---
+++ src/Infocontact.jsx
@@ -0,0 +1,26 @@
+import { prospects } from "./Prospect"
+
+export function Infocontact({ setView, idcontact }) {
+  const contact = prospects.find(p => p.id === idcontact)
+
+  if (!contact) {
+    return <h2>Contact introuvable</h2>
+  }
+
+  return (
+    <div>
+      <h1>Informations du contact</h1>
+
+      <p>Nom : {contact.name}</p>
+      <p>Email : {contact.email}</p>
+      <p>Téléphone : {contact.phone}</p>
+      <p>Source : {contact.source}</p>
+      <p>Note : {contact.note}</p>
+      <p>Status : {contact.status}</p>
+
+      <button onClick={() => setView("prospects")}>
+        Retour
+      </button>
+    </div>
+  )
+}
--- src/Prospect.jsx
+++ src/Prospect.jsx
@@ -1,33 +1,35 @@
-export  const prospects = [
+export const prospects = [
   { id: 1, name: "Mael", email: "mael@example.com", phone: "0600000000", source: "Web", note: "Client intéressé par le produit A", status: "Client" },
   { id: 2, name: "Léa", email: "lea@example.com", phone: "0611111111", source: "Salon", note: "Demande de rappel dans 1 semaine", status: "Prospect" },
   { id: 3, name: "Julien", email: "julien@example.com", phone: "0622222222", source: "Email", note: "", status: "Client" },
   { id: 4, name: "Léa", email: "luc@example.com", phone: "0611156311", source: "Salon", note: "Demande de rappel dans 1 semaine", status: "Prospect" }
-];
-export function Prospect({ setView }) {
+]
 
+export function Prospect({ setView, setSelectedContactId }) {
   return (
-
     <div>
-      <div>
-        <h1>Liste des prospects</h1>
-        <button>Ajouter un prospect  </button>
-        </div>
-      
+      <h1>Liste des prospects</h1>
 
       <ul>
         {prospects
-          .filter(prospect => prospect.status === "Prospect")
+          .filter(p => p.status === "Prospect")
           .map(prospect => (
-            <li key={prospect.id}>
-              {prospect.name} - {prospect.email} - {prospect.phone} - {prospect.status}
+            <li
+              key={prospect.id}
+              onClick={() => {
+                setSelectedContactId(prospect.id)
+                setView("infocontact")
+              }}
+            >
+              <div>
+                {prospect.name} - {prospect.email} - {prospect.phone} - {prospect.status}
+              </div>
+              
             </li>
-        ))}
+          ))}
       </ul>
 
-      <button className="Clients" onClick={() => setView("clients")}>
-        Clients
-      </button>
+      <button onClick={() => setView("clients")}>Clients</button>
     </div>
-  );
+  )
 }
