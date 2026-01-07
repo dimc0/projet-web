866072be6b334a7ad94b5e5805fd6ce7d6f22388
dimc0 <dimitri.camet@efrei.net>
2026-01-07 13:49:57 +0100

    modification

---
+++ src/Nav.jsx
@@ -0,0 +1,11 @@
+export function Nav(){
+    return (
+        <>
+        <nav>
+            <div>Prospect</div>
+            <div>Clients</div>            
+            <div>Rdv</div>
+        </nav>
+        </>
+    )
+}
